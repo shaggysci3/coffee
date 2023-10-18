62a0e161d776cb8a24de0908e9d2cbf516fb0315
Matthew Tran <matt.h.tran@gmail.com>
2023-10-18 10:05:38 -0400

    added css hot cold coffee parameters

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 //gets the hot and cold coffie div so we can append the array of data to it later
-const hotCoffees = document.getElementById('hot-coffees')
-const coldCoffees = document.getElementById('cold-coffees')
+const hotCoffees = document.getElementById("hot-coffees");
+const coldCoffees = document.getElementById("cold-coffees");
+const newCoffeeForm = document.getElementById("new-coffee");
+const coffeeDetails = document.getElementById("coffee-details");
+
+newCoffeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
 //console.log(coldCoffee)
 
 const displayCoffee = () => {
     fetch('http://localhost:3000/coffees')
     .then(resp => resp.json())
     .then((data)=> {
-        data.forEach( hotCoffee => {
+        data.forEach(hotCoffee => {
             renderHotCoffee(hotCoffee)
         });
     })
 }
+
+
 const renderHotCoffee = (hotCoffee) => {
     //create divs and img tags for the hot coffie
-    const hCoffieImg = document.createElement("img")
-    const coffieDiv = document.createElement("div")
+    const coffeeImg = document.createElement("img");
+    const coffeeDiv = document.createElement("div");
+
+    coffeeImg.src =hotCoffee.image;
+
+    coffeeDiv.appendChild(coffeeImg);
+    hotCoffees.appendChild(coffeeDiv);
 
     //augmenting the tag(img) we created
     //hCoffieImg.src = hotCoffee.
-
-
+    
 }
+
+
 displayCoffee();
