b195a301f6350f7a063daa59eac7d9f272030127
Kish <kisha_simpson@yahoo.com>
2023-10-18 14:40:17 -0500

    name changes and css

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const displayCoffee = () => {
     fetch('http://localhost:3000/coffees')
     .then(resp => resp.json())
     .then((data)=> {
-        data.forEach(hotCoffee => {
-                renderHotCoffee(hotCoffee)
+        data.forEach(coffeeDrink => {
+            renderCoffee(coffeeDrink)
         });
     })
 }
@@ -31,15 +31,15 @@ const displayCoffee = () => {
 
 
 
-    
 
 
-const renderHotCoffee = (hotCoffee) => {
+
+const renderCoffee = (coffeeDrink) => {
     //create divs and img tags for the hot coffie
 
     const coffeeImg = document.createElement("img");
-    const coffeeDiv = document.createElement("div");
-
+    const coffeeDiv = document.createElement("span");
+    coffeeDiv.setAttribute("class", "coffee-pics");
 //mouseover
     coffeeImg.addEventListener("mouseover", function (event) {
         const coffeeName = document.getElementById("coffee-name");
@@ -47,35 +47,33 @@ const renderHotCoffee = (hotCoffee) => {
         const coffeeDescription = document.getElementById("coffee-description");
         const coffeePrice = document.getElementById("coffee-price");
 
-        coffeeName.textContent = hotCoffee.name;
-        coffeeImage.src = hotCoffee.image;
-        coffeeDescription.textContent = hotCoffee.description;
-        coffeePrice.textContent = hotCoffee.price;
+        coffeeName.textContent = coffeeDrink.name;
+        coffeeImage.src = coffeeDrink.image;
+        coffeeDescription.textContent = coffeeDrink.description;
+        coffeePrice.textContent = coffeeDrink.price;
         coffeeDetails.style.display = "block";
     });
 //mouseout
 
-    coffeeImg.src = hotCoffee.image;
-    //  coffeeImg.style.width = 'width:10px';
-    //  coffeeImg.style.height - 'height:10px'
+    coffeeImg.src = coffeeDrink.image;
     coffeeImg.addEventListener("click", ()=>{
         const detailImg = document.querySelector(".coffee-image")
         const detailName = document.querySelector(".coffee-name")
         const DetailDescrip = document.querySelector(".coffee-description")
         const detailPrice = document.querySelector(".coffee-price")
     })
-    if (hotCoffee.temp === 'hot'){
+
+    if (coffeeDrink.temp === 'hot'){
         coffeeDiv.appendChild(coffeeImg);
         hotCoffees.appendChild(coffeeDiv);
-    }else if (hotCoffee.temp ==='cold') {
+    } else if (coffeeDrink.temp ==='cold') {
         coffeeDiv.appendChild(coffeeImg);
         coldCoffees.appendChild(coffeeDiv);
-
     }
 
 
     //augmenting the tag(img) we created
-    //hCoffieImg.src = hotCoffee.
+    //hCoffieImg.src = coffeeDrink.
 
 }
 
@@ -97,7 +95,7 @@ const createCoffee = () => {
         headers: { "Content-Type":"application/json" },
         body:JSON.stringify(newCoffee)
     })
-    renderHotCoffee(newCoffee)
+    renderCoffee(newCoffee)
 }
 
 displayCoffee();
